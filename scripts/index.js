15d1eebc0df2a646ae21a39871b7e1a9a86a147a
Nikita Zaharov <nix-nikita.zaharov2013@yandex.ru>
2022-04-29 22:22:18 +0300

    Проекнтная работа 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,27 @@
 const popupOpenbtn = document.querySelector('.profile__button-edit');
 const popupClosebtn = document.querySelector('.popup__button-close');
 const popup = document.querySelector('.popup');
-const popupSavebtn = document.querySelector('.popup__button-save') ;
 const formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__subname');
+let nameInput = document.querySelector('#nameInput');
+let jobInput = document.querySelector('#jobInput');
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
 
-
-popupOpenbtn.addEventListener('click',function(event){
-    popup.classList.toggle('popup_opened');
+function Openpopup(){
+    popup.classList.add('popup_opened');
+}
+function Closepopup(){
+    popup.classList.remove('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
-    event.preventDefault();
-});
+}
+    
 
-popupClosebtn.addEventListener('click',function(event){
-    popup.classList.toggle('popup_opened');
-    event.preventDefault();
-})
+popupOpenbtn.addEventListener('click',Openpopup
+);
+
+popupClosebtn.addEventListener('click',Closepopup
+);
 
 
 
@@ -27,7 +29,7 @@ function formSubmitHandler (evt){
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent= jobInput.value;
-    popup.classList.toggle('popup_opened');
+    Closepopup();
 }
  formElement.addEventListener('submit',formSubmitHandler);
 

52db204ed9739ca7fbc8b75ae68b4eba61711a73
Nikita Zaharov <nix-nikita.zaharov2013@yandex.ru>
2022-05-14 01:08:28 +0300

    Проектная работа 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,166 +1,138 @@
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-      alt:  'Aрхыз'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-      alt:  'Челябинская область'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-      alt:  'Иваново'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-      alt:  'Камчатка'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-      alt:  'Холмогорский район'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-      alt:  'Байкал'
-    }
-  ]; 
-// ОБЪЯВЛЕНИЕ КОНСТАНТ И ПЕРЕМЕННЫХ
-const popupOpenbtn = document.querySelector('.profile__button-edit');
-const popupClosebtn = document.querySelector('#editclose');
-const editPopup = document.querySelector('#editpopup');
-const formElement = document.querySelector('#formEdit');
-let nameInput = document.querySelector('#nameInput');
-let jobInput = document.querySelector('#jobInput');
-let profileName = document.querySelector('.profile__title');
-let profileJob = document.querySelector('.profile__subtitle');
-const popupPhoto = document.querySelector('#imagepopup')
-const photoCloseBtn = popupPhoto.querySelector('.popup-image__close');
-const popupPhotoTitle = document.querySelector('.popup-image__title')
-const popupPhotoLink  = document.querySelector('.popup-image__photo');
+// КОНСТАНТЫ ИЗМЕНЕНИЯ ПРОФИЛЯ
+const popupEditOpenBtn = document.querySelector('.profile__button-edit');
+const editPopup = document.querySelector('.popup_edit');
+const popupEditCloseBtn = editPopup.querySelector('.popup__button-close');
+const formEditElement = document.querySelector('#formEdit');
+const nameInput = document.querySelector('#nameInput');
+const jobInput = document.querySelector('#jobInput');
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle');
+
+//КОНАТСАНТЫ ДДЯ ПОПАПА С КАРТИНКОЙ 
+const imagePopup = document.querySelector('.popup_image');
+const imageCloseBtn = imagePopup.querySelector('.popup__button-close');
+const popupPhotoTitle = document.querySelector('.popup__title-photo');
+const popupPhotoLink  = document.querySelector('.popup__photo');
+
+// TEMPLATE 
 const photoGrid = document.querySelector('.photo-grid');
-const templateCell = document.querySelector('#cell')
-const addPopup = document.querySelector('#addpopup')
-const popupAddOpenbtn = document.querySelector('.profile__button-add');
-const popupAddClosebtn = document.querySelector('#addclose')
-const mestoInput = document.querySelector('#mestoInput');
-const linkInput = document.querySelector('#linkInput');
-const formAdd = document.querySelector('#formAdd')
+const templateCell = document.querySelector('#cell');
 
-// ОКРЫТИЕ POPUP ДЛЯ ИЗМЕНЕНИЯ ИНФОРМАЦИИ ПРОФИЛЯ
-function openpopup(){
-    editPopup.classList.add('popup_opened');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
+//КОНСТАНТЫ  ПОПАПА ДОБАВЛЕНИЯ
+const addPopup = document.querySelector('.popup_add');
+const popupAddOpenBtn = document.querySelector('.profile__button-add');
+const popupAddCloseBtn = addPopup.querySelector('.popup__button-close');
+const cardNameInput = document.querySelector('#placeInput');
+const cardLinkInput = document.querySelector('#linkInput');
+const formAdd = document.querySelector('#formAdd');
+
+// ФУНКЦИЯ ОТКРЫТИЯ POPUP
+function openPopup(anyPopup){
+  anyPopup.classList.add('popup_opened');
 }
 
-// ЗАКРЫТИЕ POPUP ДЛЯ ИЗМЕНЕНИЯ ИНФОРМАЦИИ ПРОФИЛЯ
-function closepopup(){
-    editPopup.classList.remove('popup_opened');
+// ФУНКЦИЯ ЗАКРЫТИЯ POPUP
+function closePopup(anyPopup){
+  anyPopup.classList.remove('popup_opened');
 }
-function formSubmitHandler (evt){
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileJob.textContent= jobInput.value;
-    closepopup();
-}   
-popupOpenbtn.addEventListener('click',openpopup
-);
 
-popupClosebtn.addEventListener('click',closepopup
-);
+//ФУНКЦИЯ ДЛЯ  ОТКРЫТИЯ ПОПАПА  РЕДАКТИРОВАНИЯ ПРОФИЛЯ
+function openPropfilePopup(){
+  openPopup(editPopup);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
 
-formElement.addEventListener('submit',formSubmitHandler);
+//ФУНКЦИЯ ЗАКРЫТИЯ POPUP С ИЗМЕНЕННОЙ ИНФОРМАЦИЕЙ  ПРОФИЛЯ
+function handleFormSubmitEdit (evt){
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent= jobInput.value;
+  closePopup(editPopup);
+} 
 
-// ЗАГРУЗКА 6 КАРТОЧЕК ИЗ МАССИВА НА СТРАНИЦУ
-initialCards.forEach(function(item){
-  let cellGrid = templateCell.content.cloneNode(true) 
-  let cellLink = cellGrid.querySelector('.cell__photo');  
-  let cellName = cellGrid.querySelector('.cell__info-title');
-  let cellPhoto = cellGrid.querySelector('.cell__photo')
-  photoGrid.append(cellGrid)
-  cellName.textContent = item.name;
-  cellLink.src = item.link;
-  cellLink.alt = item.alt;
-  cellPhoto.addEventListener('click',photoPopupOpen)
+// ОБРАБОТЧИКИ ДЛЯ ПРОФИЛЯ
+popupEditOpenBtn.addEventListener('click', openPropfilePopup);
+popupEditCloseBtn.addEventListener('click', function(){
+  closePopup(editPopup);
+})
+formEditElement.addEventListener('submit', handleFormSubmitEdit);
+
+// ФУНКЦИЯ УДАЛЕНИЯ КАРТОЧКИ
+function handleDeleteCell (evt){
+  evt.target.closest('.cell').remove()
+}
+
+// ФУНКЦИЯ ПРОСТАВЛЕНИЯ ЛАЙКА
+function handleLikeCell (evt) {
+  evt.target.classList.toggle('cell__button-like_active')
+}
+
+// ФУНКЦИЯ ДЛЯ ОТКРЫТЯ ПОПАПА С КАРТИНКОЙ
+function openImagePopup(evt) {
+  const linkImagePopup = evt.target.getAttribute('src');
+  const titleImagePopup = evt.target.getAttribute('alt');
+  popupPhotoLink.src = linkImagePopup;
+  popupPhotoTitle.textContent = titleImagePopup;
+  openPopup(imagePopup);
+}
+
+// ФУНКЦИЯ СОЗДАНИЯ КАРТОЧКИ
+function createCard ({name, link, alt}) {
+  const cellGrid = templateCell.content.cloneNode(true) ;
+  const cellPhoto = cellGrid.querySelector('.cell__photo');  
+  const cellName = cellGrid.querySelector('.cell__info-title');
+  cellName.textContent = name;
+  cellPhoto.src = link;
+  cellPhoto.alt = alt;
+
+  const deleteButton = cellGrid.querySelector('.cell__button-delete');
+  deleteButton.addEventListener('click', handleDeleteCell);
+
+  const likeButton = cellGrid.querySelector('.cell__button-like');
+  likeButton.addEventListener('click', handleLikeCell);
+
+  cellPhoto.addEventListener('click',openImagePopup);
+
+  return cellGrid
+}
+
+// ФУНКИЯ ДОБАВЛЕНИЯ 6 КАРТОЧЕК ИЗ МАССИВА
+initialCards.forEach(({name, link, alt}) => {
+  photoGrid.append(createCard({name, link, alt}))
 })
 
-// ОТКРЫТИЕ POPUP ДЛЯ ДОБАВЛЕНИЯ КАРТОЧЕК
+//ФУНКЦИЯ ОТКРЫТИЯ POPUP ДЛЯ ДОБАВЛЕНИЯ КАРТОЧЕК
 function openAddPopup(){
-    addPopup.classList.add('popup_opened');
-    mestoInput.value =''
-    linkInput.value =''
-}
-
-// ЗАКРЫТИЕ POPUP ДЛЯ ДОБАВЛЕНИЯ КАРТОЧЕК
-function closeAddPopup(){
-    addPopup.classList.remove('popup_opened');
+  openPopup(addPopup)
 }
 
 // ДОБАВЛЕНИЕ НОВОЙ КАРТОЧКИ
-function formSubmitAdd (evt){
+function handleFormSubmitAdd (evt){
   evt.preventDefault();
-  cellGrid = templateCell.content.cloneNode(true)
-  cellLink = cellGrid.querySelector('.cell__photo');  
-  cellName = cellGrid.querySelector('.cell__info-title');
-  let cellButtonLike = cellGrid.querySelector('.cell__button-like');
-  let cellButtonDelete = cellGrid.querySelector('.cell__button-delete') 
-  cellPhoto = cellGrid.querySelector('.cell__photo')
-  photoGrid.prepend(cellGrid)
-  cellName.textContent = mestoInput.value;
-  cellLink.src = linkInput.value
-  cellLink.alt = mestoInput.value
-  cellButtonLike.addEventListener('click',function(){
-  cellButtonLike.classList.toggle('cell__button-like_active')
-  })
-  cellButtonDelete.addEventListener('click', function(evt){
-    evt.target.closest('.cell').remove()
-  })
-  cellPhoto.addEventListener('click',photoPopupOpen)
-  closeAddPopup();  
+  photoGrid.prepend(createCard({
+      name: cardNameInput.value,
+      link: cardLinkInput.value,
+      alt: cardNameInput.value
+    }))
+  formAdd.reset();
+  closePopup(addPopup);  
 } 
 
-popupAddOpenbtn.addEventListener('click', openAddPopup);
-popupAddClosebtn.addEventListener('click', closeAddPopup)
-formAdd.addEventListener('submit', formSubmitAdd);
-
-// ЗАКРЫТИЕ POPUP С КАРТИНКОЙ
-photoCloseBtn.addEventListener('click',function(){
-  popupPhoto.classList.remove('popup_opened')
+popupAddOpenBtn.addEventListener('click', openAddPopup);
+popupAddCloseBtn.addEventListener('click',function(){
+  closePopup(addPopup)
+})
+formAdd.addEventListener('submit', handleFormSubmitAdd);
+imageCloseBtn.addEventListener('click',function(){
+  closePopup(imagePopup)
 })
 
-// ФУНКЦИЯ ОТКРЫТИЯ POPUP С КАРТИНКОЙ
-function photoPopupOpen(evt) {
-  popupPhoto.classList.toggle('popup_opened');
-  const linkImagePopup = evt.target.getAttribute('src')
-  const titleImagePopup = evt.target.getAttribute('alt')
-  console.log(linkImagePopup)
-  popupPhotoLink.src = linkImagePopup;
-  popupPhotoTitle.textContent = titleImagePopup
-}
 
-// КНОПКА LIKE
-const buttonLike = document.querySelectorAll('.cell__button-like');
-const buttonLikeArray = Array.from(buttonLike)
-buttonLikeArray.forEach(function(el){
-  el.addEventListener('click', function(evt){
-    evt.target.classList.toggle('cell__button-like_active');
-  })
-})
 
-// УДАЛЕНИЕ КАРТОЧЕК
-const buttonDelete = document.querySelectorAll('.cell__button-delete');
-const buttonDeleteArray = Array.from(buttonDelete);
-buttonDeleteArray.forEach(function(item){
-    item.addEventListener('click',function(evt){
-      evt.target = item.closest('.cell').remove();
-    })
-})
+
+
+
 
 
 
